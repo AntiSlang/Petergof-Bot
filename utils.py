91d1f243692e346a9d7026fc23e5613b48c4a606
Daniil <114076646+DanBel1kov@users.noreply.github.com>
2025-01-13 22:55:46 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import json
 
-
 def create_chunks(file_path):
 
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -9,21 +8,60 @@ def create_chunks(file_path):
     docs = []
 
     for place in data['places']:
-        all_info_text = f"""
-        Название: 
-        {place['title']}
+        all_info_text = f"""Название: 
+{place['title']}
 
-        Информация об объекта: 
-        {place['context']}
+Информация об объекта: 
+{place['context']}
 
-        Оценка объекта (по 100 бальной шкале):
-        {place['rate']}
+Оценка объекта (по 100 бальной шкале):
+{place['rate']}
 
-        Ссылка на объект:
-        {place['full_url']}
-        """
+Ссылка на объект:
+{place['full_url']}
+"""
+
+        if len(place['c_real']) > 1:
+            all_info_text += f"""Координты объекта:
+Широта (lat): {place['c_real'][0]}
+Долгота (lon): {place['c_real'][1]}
+"""
 
         docs.append(all_info_text)
 
     return docs
+
+
+def create_json_chunks(file_path='data.json'):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    data_chunks = []
+
+    for place in data['places']:
+        name = place['title']
+        description = place['context']
+        score = place['rate']
+        link = place['full_url']
+
+        if len(place['c_real']) > 1:
+            coordinates = {
+                'lat': place['c_real'][0],
+                'lon': place['c_real'][1]
+            }
+        else:
+            continue
+
+        chunk = {
+            'name': name,
+            'description': description,
+            'score': score,
+            'link': link,
+            'coordinates': coordinates
+        }
+
+        data_chunks.append(chunk)
+
+    return data_chunks
+
 
